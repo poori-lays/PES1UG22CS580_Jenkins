39e7bbb76bdd6f69f89aa28db6efbccf7a0a8dcb
Shreyas_KM <141303779+poori-lays@users.noreply.github.com>
2025-03-13 11:07:58 +0000

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout<<"Shreyas Karthik M";
+  cout<<"Shreyas Karthik M karthik";
+  int a=8;
   return 0;
 }
